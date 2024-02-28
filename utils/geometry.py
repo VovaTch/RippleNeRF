f10ecbbd6a9f4a702e1ceff0e6586d4be6777f87
Tchuiev Vladimir (CR/RAR1-IL) <tcv1tv@TV2ZOSD19.hfa.emea.bosch.com>
2024-02-28 17:24:48 +0200

    Creating the Ray object

--- utils/geometry.py
+++ utils/geometry.py
@@ -6,8 +6,33 @@ import numpy as np
 
 @dataclass
 class Ray:
+    """
+    Describes a ray in 3D space.
+
+    Fields:
+    - origin (np.ndarray): 3D point
+    - direction (np.ndarray): 3D vector
+    """
+
     origin: np.ndarray
     direction: np.ndarray
+
+    @property
+    def ray_tensor(self) -> torch.Tensor:
+        """
+        Returns a tensor representation of the ray.
+
+        Returns:
+            torch.Tensor: A tensor representing the ray, consisting of the origin, yaw angle, and pitch angle.
+        """
+        origin_tensor = torch.tensor(self.origin)
+        direction_tensor = torch.tensor(self.direction)
+        yaw_angle = torch.atan2(torch.tensor(direction_tensor[1], direction_tensor[0]))
+        pitch_angle = torch.atan2(
+            torch.tensor(direction_tensor[2]),
+            torch.norm(direction_tensor[1], direction_tensor[0]),
+        )
+        return torch.cat([origin_tensor, yaw_angle, pitch_angle])
 
 
 @dataclass
